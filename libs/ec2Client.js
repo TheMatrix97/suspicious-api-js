fbd6db0dd4734cd1802f789924174ea3686581eb
Marc Catrisse <marc@catrisse.net>
2025-11-13 23:05:10 +0100

    Use environment variable for EC2 region

--- libs/ec2Client.js
+++ libs/ec2Client.js
@@ -1,4 +1,4 @@
 const {EC2Client, RunInstancesCommand, TerminateInstancesCommand} = require("@aws-sdk/client-ec2");
-const REGION = "us-east-1";
+const REGION = process.env.REGION ?? "us-east-1";
 const ec2Client = new EC2Client({ region: REGION });
 module.exports = { ec2Client, RunInstancesCommand, TerminateInstancesCommand };
